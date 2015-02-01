dc7a4972b37e478e81b5b29d358e06174d93299e
kelvinhsung <ksung@u.washington.edu>
2015-02-01 00:19:18 -0800

    Forgot to remove .bak files and checkin xmlloader

---
+++ BookChapters/Chapter4/4.6.AudioSupport/public_html/src/Engine/Core/Resources/Engine_XmlLoader.js
@@ -0,0 +1,53 @@
+/*
+ * File: EngineCore_XmlLoader.js 
+ * loads an XML file into ResoruceMap.
+ */
+
+"use strict";  // Operate in Strict mode such that variables must be declared before used!
+
+var gEngine = gEngine || { };
+
+// Note: loads the XMLfile and when done calls the callbackFunction()
+//     fileName is treated as resource map key, file content is stored as asset
+//
+gEngine.XmlLoader = function()
+{        
+    var LoadXMLFile= function(fileName, callbackFunction) {
+        if (!(gEngine.ResourceMap.IsAssetLoaded(fileName)))
+        {
+            // Update resources in load counter.
+            gEngine.ResourceMap.AsyncLoadRequested(fileName);
+                        
+            // Asyncrounsly request the data from server.
+            var req = new XMLHttpRequest();
+            req.open('GET', fileName, true);
+            req.setRequestHeader('Content-Type', 'text/xml');
+            
+            req.onload = function () 
+            {
+                var parser = new DOMParser();
+                var fileContent = parser.parseFromString(req.responseText, "text/xml");
+                gEngine.ResourceMap.AsyncLoadCompleted(fileName, fileContent);
+                callbackFunction(fileName);
+            };
+            req.send();
+            
+        } else {
+            gEngine.ResourceMap.IncAssetRefCount(fileName);
+            callbackFunction(fileName);
+        }
+    };
+    
+    var UnloadXMLFile= function(fileName) {
+        gEngine.ResourceMap.DecAssetRefCount(fileName);
+    };
+    
+    // Public interface for this object. Anything not in here will
+    // not be accessable.
+    var oPublic =
+    {
+        LoadXMLFile: LoadXMLFile,
+        UnloadXMLFile: UnloadXMLFile
+    };
+    return oPublic;
+}();

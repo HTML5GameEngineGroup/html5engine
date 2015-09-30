ec3afd55d6cd22a1c05b50f0c06564556d8b9ea9
kelvinhsung <ksung@u.washington.edu>
2015-09-29 20:25:49 -0700

    Remove unused template projects

--- SamplePlatformer/public_html/src/MyGame/Objects/Platforms/StaticHPlatform.js
+++
@@ -1,8 +0,0 @@
-/* 
- * These guys have constant thickness
- */
-function StaticHPlatform(cx, cy, size)
-{
-    PlatformObject.call(this, cx, cy, size, _gPlatformThickness);
-}
-gEngine.Core.InheritPrototype(StaticHPlatform, StaticPlatform);
--- SamplePlatformer/public_html/src/MyGame/Objects/Platforms/StaticVPlatform.js
+++
@@ -1,8 +0,0 @@
-/* 
- * These guys have constant thickness
- */
-function StaticVPlatform(cx, cy, size)
-{
-    PlatformObject.call(this, cx, cy, _gPlatformThickness, size);
-}
-gEngine.Core.InheritPrototype(StaticVPlatform, StaticPlatform);

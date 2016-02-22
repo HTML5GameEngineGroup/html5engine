2c38b7eea257ba4e31016378041955d7007bff34
David Watson <dewats7@uw.edu>
2016-02-22 00:22:18 -0800

    New Parallax Sample 6c
    - Created a new parallax sample code using camera panning.
    - Corrected spelling of keyboard in tutorial 2

--- 2016.1.DavidWatson/tutorial_source/src/MyGame/MyGameScene6c.js
+++ 2016.1.DavidWatson/tutorial_source/src/MyGame/MyGameScene6c.js
@@ -3,153 +3,104 @@
  * This is the logic of our game. 
  */
 
-"use strict"; 
+"use strict";
 
 function MyGameScene() {
     this.mCamera = null;
+    this.mLight = null;
     
-    this.kLayer1 = "assets/layer_01.png";
-    this.kLayer2 = "assets/layer_02.png";
-    this.kLayer3 = "assets/layer_03.png";
-    this.kLayer4 = "assets/layer_04.png";
-    this.kLayer5 = "assets/layer_05.png";
-    this.kLayer6 = "assets/layer_06.png";
-    this.kLayer7 = "assets/layer_07.png";
-    this.kLayer8 = "assets/layer_08.png";
-	
     this.kTexture = "assets/minion_spritesheet.png"
+    this.kBgTexture = "assets/bg.png"
+    this.kBgTextureNormal = "assets/bg_normal.png"
+    this.kBg2Texture = "assets/bgLayer.png"
+    this.kBg2TextureNormal = "assets/bgLayer_normal.png"
 }
 gEngine.Core.inheritPrototype(MyGameScene, Scene);
 
 MyGameScene.prototype.loadScene = function () {
-	gEngine.Textures.loadTexture(this.kLayer1);
-	gEngine.Textures.loadTexture(this.kLayer2);
-	gEngine.Textures.loadTexture(this.kLayer3);
-	gEngine.Textures.loadTexture(this.kLayer4);
-	gEngine.Textures.loadTexture(this.kLayer5);
-	gEngine.Textures.loadTexture(this.kLayer6);
-	gEngine.Textures.loadTexture(this.kLayer7);
-	gEngine.Textures.loadTexture(this.kLayer8);
     gEngine.Textures.loadTexture(this.kTexture);
+    gEngine.Textures.loadTexture(this.kBgTexture);
+    gEngine.Textures.loadTexture(this.kBgTextureNormal);
+    gEngine.Textures.loadTexture(this.kBg2Texture);
+    gEngine.Textures.loadTexture(this.kBg2TextureNormal);
 };
 
 
 MyGameScene.prototype.unloadScene = function () {
-	gEngine.Textures.unloadTexture(this.kLayer1);
-	gEngine.Textures.unloadTexture(this.kLayer2);
-	gEngine.Textures.unloadTexture(this.kLayer3);
-	gEngine.Textures.unloadTexture(this.kLayer4);
-	gEngine.Textures.unloadTexture(this.kLayer5);
-	gEngine.Textures.unloadTexture(this.kLayer6);
-	gEngine.Textures.unloadTexture(this.kLayer7);
-	gEngine.Textures.unloadTexture(this.kLayer8);
     gEngine.Textures.unloadTexture(this.kTexture);
+    gEngine.Textures.unloadTexture(this.kBgTexture);
+    gEngine.Textures.unloadTexture(this.kBgTextureNormal);
+    gEngine.Textures.unloadTexture(this.kBg2Texture);
+    gEngine.Textures.unloadTexture(this.kBg2TextureNormal);
 };
 
-MyGameScene.prototype.initialize  = function () {
+MyGameScene.prototype.initialize = function () {
     this.mCamera = new Camera(
-        vec2.fromValues(50, 40),		// position of the camera
-        100,							// width of camera
-        [0, 0, 500, 400]				// viewport (orgX, orgY, width, height)
-    );
+            vec2.fromValues(50, 40), // position of the camera
+            100, // width of camera
+            [0, 0, 500, 400]				// viewport (orgX, orgY, width, height)
+            );
     this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
     
-    var bg = new LightRenderable(this.kLayer1);
-    bg.getXform().setSize(200, 100);
-    bg.getXform().setPosition(50, 40);
-    bg.getXform().setZPos(0); 
-    this.mBgL1 = new ParallaxGameObject(bg, 1, this.mCamera);
-	
-    bg = new LightRenderable(this.kLayer2);
-    bg.getXform().setSize(200, 100);
-    bg.getXform().setPosition(50, 40);
-    bg.getXform().setZPos(0); 
-    this.mBgL2 = new ParallaxGameObject(bg, 1, this.mCamera);
-	
     
-    bg = new LightRenderable(this.kLayer3);
-    bg.getXform().setSize(200, 100);
-    bg.getXform().setPosition(50, 40);
-    bg.getXform().setZPos(0); 
-    this.mBgL3 = new ParallaxGameObject(bg, 3, this.mCamera);
-	
-    bg = new LightRenderable(this.kLayer4);
-    bg.getXform().setSize(200, 100);
-    bg.getXform().setPosition(50, 40);
-    bg.getXform().setZPos(0); 
-    this.mBgL4 = new ParallaxGameObject(bg, 4, this.mCamera);
-	
-    bg = new LightRenderable(this.kLayer5);
-    bg.getXform().setSize(200, 100);
-    bg.getXform().setPosition(50, 40);
-    bg.getXform().setZPos(0); 
-    this.mBgL5 = new ParallaxGameObject(bg, 5, this.mCamera);
-	
-    bg = new LightRenderable(this.kLayer6);
-    bg.getXform().setSize(200, 100);
-    bg.getXform().setPosition(50, 40);
-    bg.getXform().setZPos(0); 
-    this.mBgL6 = new ParallaxGameObject(bg, 6, this.mCamera);
+    this.mLight = new Light();
+    this.mLight.setLightType(Light.eLightType.eDirectionalLight);
+    this.mLight.setColor([1.0, 1.0, 1.0, 1]);
+    this.mLight.setDirection([0.3, 0, -1]);
+    this.mLight.setIntensity(1);
     
-    bg = new LightRenderable(this.kLayer7);
-    bg.getXform().setSize(175, 100);
-    bg.getXform().setPosition(100, 40);
-    bg.getXform().setZPos(0); 
-    this.mBgL7 = new ParallaxGameObject(bg, 7, this.mCamera);
-	
-    bg = new LightRenderable(this.kLayer8);
-    bg.getXform().setSize(200, 100);
-    bg.getXform().setPosition(50, 40);
-    bg.getXform().setZPos(0); 
-    this.mBgL8 = new ParallaxGameObject(bg, 8, this.mCamera);
-	
     // we now use a sprite renderable object
-    this.mRenderable = new SpriteRenderable(this.kTexture);
+    this.mRenderable = new LightRenderable(this.kTexture);
     this.mRenderable.setElementPixelPositions(130, 310, 0, 180);
+    this.mRenderable.addLight(this.mLight);
     
     // create a new game object with the new renderable
     this.mGameObject = new GameObject(this.mRenderable);
     this.mGameObject.getXform().setSize(16, 16);
     this.mGameObject.getXform().setPosition(30, 50);
+
+    var bg = new IllumRenderable(this.kBgTexture,this.kBgTextureNormal);
+    bg.getXform().setSize(100, 100);
+    bg.getXform().setPosition(50, 40);
+    bg.getXform().setZPos(0);
+    this.mBgL1 = new ParallaxGameObject(bg, 2, this.mCamera);
+    bg.addLight(this.mLight);
     
-    gEngine.DefaultResources.setGlobalAmbientIntensity(3);
+    bg = new IllumRenderable(this.kBg2Texture,this.kBg2TextureNormal);
+    bg.getXform().setSize(100, 100);
+    bg.getXform().setPosition(50, 40);
+    bg.getXform().setZPos(0);
+    this.mBgL2 = new ParallaxGameObject(bg, 1, this.mCamera);
+    bg.addLight(this.mLight);
+    
+    
+    gEngine.DefaultResources.setGlobalAmbientIntensity(0);
 };
 
 // This is the draw function, make sure to setup proper drawing environment, and more
 // importantly, make sure to _NOT_ change any state.
 MyGameScene.prototype.draw = function () {
     // Clear the screen
-	gEngine.Core.clearCanvas([0.8, 0.8, 0.8, 1.0]);
-    
-	// Activate our camera
-	this.mCamera.setupViewProjection();
-    
-	// Draw our objects
-	this.mBgL8.draw(this.mCamera);
-	this.mBgL7.draw(this.mCamera);
-	this.mBgL6.draw(this.mCamera);
-	this.mBgL5.draw(this.mCamera);
-	this.mBgL4.draw(this.mCamera);
-	this.mBgL3.draw(this.mCamera);
-	this.mBgL2.draw(this.mCamera);
-	this.mBgL1.draw(this.mCamera);
-	
+    gEngine.Core.clearCanvas([0.8, 0.8, 0.8, 1.0]);
+
+    // Activate our camera
+    this.mCamera.setupViewProjection();
+
+    // Draw our objects
+    this.mBgL1.draw(this.mCamera);
+    this.mBgL2.draw(this.mCamera);
+
     this.mGameObject.draw(this.mCamera);
 };
 
 // The Update function, updates the application state. Make sure to _NOT_ draw
 // anything from this function!
 MyGameScene.prototype.update = function () {
-	this.mBgL1.update();
-	this.mBgL2.update();
-	this.mBgL3.update();
-	this.mBgL4.update();
-	this.mBgL5.update();
-	this.mBgL6.update();
-	this.mBgL7.update();
-	this.mBgL8.update();
-	
-	if (gEngine.Input.isKeyPressed(gEngine.Input.keys.A)) {
+    
+    this.mBgL1.update();
+    this.mBgL2.update();
+
+    if (gEngine.Input.isKeyPressed(gEngine.Input.keys.A)) {
         this.mRenderable.setElementPixelPositions(130, 310, 0, 180);
         this.mGameObject.getXform().incXPosBy(-0.5);
     }
@@ -158,6 +109,15 @@ MyGameScene.prototype.update = function () {
         this.mRenderable.setElementPixelPositions(720, 900, 0, 180);
         this.mGameObject.getXform().incXPosBy(0.5);
     }
+
+    if (gEngine.Input.isKeyPressed(gEngine.Input.keys.S)) {
+        this.mGameObject.getXform().incYPosBy(-0.5);
+    }
+
+    if (gEngine.Input.isKeyPressed(gEngine.Input.keys.W)) {
+        this.mGameObject.getXform().incYPosBy(0.5);
+    }
+    
     this.mGameObject.update();
     this.mCamera.panWith(this.mGameObject.getXform(), 0.9);
     this.mCamera.update();
